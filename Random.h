6c562c4f09c62f12eac20ed687b6d544aee34c46
Gabriel <gaboutch@outlook.com>
2023-12-01 15:33:35 -0500

    Update Random.h

--- Random.h
+++ Random.h
@@ -5,8 +5,12 @@
 namespace utils {
 	class Random {
 	public:
-		static float floatRange(float min, float max);
-
-		static int intRange(int min, int max);
+		static float floatRange(float min, float max) {
+        		return min + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (max - min)));
+    		}
+	
+    		static int intRange(int min, int max) {
+        		return min + (rand() % (max - min));
+    		}
 	};
 }
