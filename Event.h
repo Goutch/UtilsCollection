7e0b1b842b5ba809170b04123ddebfad1fab5db8
Gabriel <gaboutch@outlook.com>
2026-02-27 23:17:17 -0500

    fix things

--- Event.h
+++ Event.h
@@ -6,21 +6,24 @@
 #include "unordered_set"
 #include "functional"
 #include "unordered_map"
-#include "HandleContainer.h"
+#include "data-structure/StableHandleContainer.h"
 
-namespace HBE {
+namespace HBE
+{
     using event_subscription_id = Handle;
 
-    template<typename... Args>
-    class Event {
-        struct Listener {
+    template <typename... Args>
+    class Event
+    {
+        struct Listener
+        {
             event_subscription_id id;
             int priority;
             std::function<void(Args...)> callback;
         };
 
-        HandleContainer<Listener> container;
-        std::vector<Listener> sorted_listeners;
+        StableHandleContainer<Listener, 4> container;
+        RawVector<Listener> sorted_listeners;
         size_t next_id = 0;
         uint32_t invoke_index = 0;
 
@@ -30,87 +33,101 @@ namespace HBE {
     public:
         Event() = default;
 
-        Event(const Event &) = delete;
+        Event(const Event&) = delete;
 
-        Event &operator=(const Event &) = delete;
+        Event& operator=(const Event&) = delete;
 
-        Event(Event &&other)
-
-            noexcept {
+        Event(Event&& other) noexcept
+        {
             container = std::move(other.container);
             sorted_listeners = std::move(other.sorted_listeners);
         }
 
-        Event &operator=(Event &&other)
-
-            noexcept {
+        Event& operator=(Event&& other) noexcept
+        {
             container = std::move(other.container);
             sorted_listeners = std::move(other.sorted_listeners);
             return *this;
         }
 
-        void subscribe(event_subscription_id &id, std::function<void(Args...)> function, int priority) {
+        void subscribe(event_subscription_id& id, std::function<void(Args...)> function, int priority)
+        {
             id = container.create();
             container.set(id, Listener{id, priority, std::move(function)});
             sorted_listeners.push_back(container.get(id));
             std::stable_sort(sorted_listeners.begin(), sorted_listeners.end(),
-                             [](auto &a, auto &b) { return a.priority < b.priority; });
+                             [](auto& a, auto& b) { return a.priority < b.priority; });
         }
 
-        bool valid(event_subscription_id id) {
+        bool valid(event_subscription_id id)
+        {
             return container.valid(id);
         }
 
-        void subscribe(event_subscription_id &id, void (*fn)(Args...), int priority = 0) {
+        void subscribe(event_subscription_id& id, void (*fn)(Args...), int priority = 0)
+        {
             subscribe(id,
                       [fn](Args... args) { fn(args...); },
                       priority
             );
         }
 
-        template<typename T>
-        void subscribe(event_subscription_id &id, T *instance, void (T::*method)(Args...), int priority = 0) {
+        template <typename T>
+        void subscribe(event_subscription_id& id, T* instance, void (T::*method)(Args...), int priority = 0)
+        {
             subscribe(id,
-                      [instance, method](Args... args) {
+                      [instance, method](Args... args)
+                      {
                           (instance->*method)(args...);
                       },
                       priority
             );
         }
 
-        void unsubscribe(event_subscription_id id) {
-            if (container.valid(id)) {
+        void unsubscribe(event_subscription_id id)
+        {
+            if (container.valid(id))
+            {
                 container.release(id);
-                for (int i = sorted_listeners.size() - 1; i >= 0; --i) {
-                    if (sorted_listeners[i].id == id) {
+                for (int i = sorted_listeners.size() - 1; i >= 0; --i)
+                {
+                    if (sorted_listeners[i].id == id)
+                    {
                         sorted_listeners.erase(sorted_listeners.begin() + i);
                         //make sure that deletion of the listener does not affect the invoke loop
-                        if (invoke_index >= i) {
+                        if (invoke_index >= i)
+                        {
                             invoke_index--;
                         }
                         break;
                     }
                 }
-            } else {
+            }
+            else
+            {
                 printf("Trying to unsubscribe from an event with invalid subscription id ");
             }
         }
 
-        void consume() {
+        void consume()
+        {
             consumed = true;
         }
 
-        template<typename... CallArgs>
-        void invoke(CallArgs &&... args) {
+        template <typename... CallArgs>
+        void invoke(CallArgs&&... args)
+        {
             consumed = false;
-            for (invoke_index = 0; invoke_index < sorted_listeners.size(); ++invoke_index) {
+            for (invoke_index = 0; invoke_index < sorted_listeners.size(); ++invoke_index)
+            {
                 if (consumed)
                     break;
                 sorted_listeners[invoke_index].callback(std::forward<CallArgs>(args)...);
             }
         }
 
-        uint32_t listenerCount() const {
+        uint32_t listenerCount() const
+        {
             return sorted_listeners.size();
         }
     };
--- data-structure/Handle.h
+++ data-structure/Handle.h
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-
-#pragma once
-namespace HBE{
-	struct Handle {
-		uint32_t index = std::numeric_limits<uint32_t>::max();
-		uint32_t version = std::numeric_limits<uint32_t>::max();
-
-		bool operator==(Handle other) const {
-			return index == other.index && version == other.version;
-		}
-	};
-=======
 #pragma once
 namespace HBE
 {
@@ -24,5 +11,4 @@ namespace HBE
             return index == other.index && version == other.version;
         }
     };
->>>>>>> Stashed changes
 }
