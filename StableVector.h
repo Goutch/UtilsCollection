481a4471fce0fdcdb162727a463205db8bd0f92a
Gabriel <gaboutch@outlook.com>
2026-02-27 22:26:22 -0500

    Create StableVector.h

---
+++ StableVector.h
@@ -0,0 +1,129 @@
+#pragma once
+#include <bit>
+#include "Vector.h"
+
+namespace HBE
+{
+    template <typename T, size_t PAGE_SIZE>
+    class StableVector
+    {
+        static_assert(std::is_trivially_copyable<T>(), "T must be trivially copyable");
+        static_assert(PAGE_SIZE > 0, "Page size must be greater than 0");
+        static_assert(PAGE_SIZE > 0 && (PAGE_SIZE & (PAGE_SIZE - 1)) == 0, "Page size must be power of 2");
+        static constexpr size_t SHIFT = std::countr_zero(PAGE_SIZE);
+        static constexpr size_t MASK = PAGE_SIZE - 1;
+
+        struct Page
+        {
+            T data[PAGE_SIZE];
+        };
+
+        Vector<Page*> pages;
+        size_t m_size = 0;
+
+    private :
+        inline size_t getPageIndex(size_t i)
+        {
+            return i >> SHIFT;
+        }
+
+        inline size_t getLocalIndex(size_t i)
+        {
+            return i & MASK;
+        }
+
+    public:
+        void clear()
+        {
+            for (int i = 0; i < pages.size(); i++)
+            {
+                delete pages[i];
+            }
+            pages.clear();
+            m_size = 0;
+        }
+
+        StableVector() = default;
+
+        ~StableVector()
+        {
+            clear();
+        }
+
+        StableVector(StableVector&& Other) noexcept
+        {
+            pages = std::move(Other.pages);
+            Other.clear();
+        }
+
+        StableVector(const StableVector& other) = delete;
+        StableVector& operator=(const StableVector& other) = delete;
+
+        void push_back(const T& item)
+        {
+            reserve(m_size + 1);
+            (*this)[m_size] = item;
+            m_size++;
+        }
+
+        void emplace_back(const T& item)
+        {
+            push_back(item);
+        }
+
+        void reserve(size_t new_capacity)
+        {
+            if (new_capacity <= capacity())
+                return;
+
+            size_t required_pages = getPageIndex(new_capacity);
+
+            while (pages.size() < required_pages)
+                pages.push_back(new Page());
+        }
+
+        void resize(size_t new_size)
+        {
+            if (new_size > m_size)
+            {
+                reserve(new_size);
+
+                // zero initialize new elements (since trivially copyable)
+                size_t old_size = m_size;
+                m_size = new_size;
+
+                for (size_t i = old_size; i < new_size; ++i)
+                    (*this)[i] = T{};
+            }
+            else
+            {
+                // shrinking only changes logical size (stable storage)
+                m_size = new_size;
+            }
+        }
+
+        size_t size()
+        {
+            return m_size;
+        }
+
+        size_t capacity() const
+        {
+            return pages.size() * PAGE_SIZE;
+        }
+
+        T& operator[](size_t i)
+        {
+            const size_t page_index = getPageIndex(i);
+            const size_t local_index = getLocalIndex(i);
+            return pages[page_index]->data[local_index];
+        }
+
+        const T& operator[](size_t i) const
+        {
+            const size_t page_index = getPageIndex(i);
+            const size_t local_index = getLocalIndex(i);
+            return pages[page_index]->data[local_index];
+        }
+    };
+}
