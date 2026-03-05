72c67523887d1866769555129c90cf0de12445ca
Gabriel Bouchard <33035583+Goutch@users.noreply.github.com>
2026-03-04 22:30:59 -0500

    stable vector revert and handle modifs

--- data-structure/Handle.h
+++ data-structure/Handle.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <limits>
 #include <cstdint>
-#define HBE_NULL_HANDLE std::numeric_limits<uint64_t>::max();
 
 namespace HBE
 {
+
     typedef uint64_t hbe_handle_t;
 	using ImageHandle = hbe_handle_t;
 	typedef hbe_handle_t Handle;
+	constexpr Handle HBE_NULL_HANDLE = std::numeric_limits<uint64_t>::max();
 }
--- data-structure/StableVector.h
+++ data-structure/StableVector.h
@@ -1,42 +1,34 @@
 #pragma once
 #include "RawVector.h"
 
-namespace HBE
-{
-    template <typename T, size_t PAGE_SIZE>
-    class StableVector
-    {
+namespace HBE {
+    template<typename T, size_t PAGE_SIZE>
+    class StableVector {
         static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
         static_assert(PAGE_SIZE > 0, "Page size must be greater than 0");
         static_assert(PAGE_SIZE > 0 && (PAGE_SIZE & (PAGE_SIZE - 1)) == 0, "Page size must be power of 2");
 
-        constexpr size_t log2_constexpr(size_t n, size_t count = 0)
-        {
+        static constexpr size_t log2_constexpr(size_t n, size_t count = 0) {
             return (n <= 1) ? count : log2_constexpr(n >> 1, count + 1);
         }
 
-        static constexpr size_t SHIFT = log2_constexpr(PAGE_SIZE);
+        static constexpr size_t SHIFT = log2_constexpr(PAGE_SIZE, 0);
         static constexpr size_t MASK = PAGE_SIZE - 1;
-
         RawVector<T[PAGE_SIZE]> pages;
         size_t m_size = 0;
 
     private :
-        inline size_t getPageIndex(size_t i)
-        {
+        inline size_t getPageIndex(size_t i) {
             return i >> SHIFT;
         }
 
-        inline size_t getLocalIndex(size_t i)
-        {
+        inline size_t getLocalIndex(size_t i) {
             return i & MASK;
         }
 
     public:
-        void clear()
-        {
-            for (int i = 0; i < pages.size(); i++)
-            {
+        void clear() {
+            for (int i = 0; i < pages.size(); i++) {
                 delete pages[i];
             }
             pages.clear();
@@ -45,45 +37,39 @@ namespace HBE
 
         StableVector() = default;
 
-        ~StableVector()
-        {
+        ~StableVector() {
             clear();
         }
 
-        StableVector(StableVector&& Other) noexcept
-        {
+        StableVector(StableVector &&Other) noexcept {
             pages = std::move(Other.pages);
             Other.clear();
         }
 
-        StableVector(const StableVector& other) = delete;
-        StableVector& operator=(const StableVector& other) = delete;
+        StableVector(const StableVector &other) = delete;
 
-        void push_back(const T& item)
-        {
+        StableVector &operator=(const StableVector &other) = delete;
+
+        void push_back(const T &item) {
             reserve(m_size + 1);
             (*this)[m_size] = item;
             m_size++;
         }
 
-        void emplace_back(const T& item)
-        {
+        void emplace_back(const T &item) {
             push_back(item);
         }
 
-        void erase(size_t i)
-        {
+        void erase(size_t i) {
             if (i >= m_size)
                 return;
-            if (i == size - 1)
-            {
+            if (i == m_size - 1) {
                 m_size--;
             }
             (*this)[i] = {};
         }
 
-        void reserve(size_t new_capacity)
-        {
+        void reserve(size_t new_capacity) {
             if (new_capacity <= capacity())
                 return;
 
@@ -93,10 +79,8 @@ namespace HBE
                 pages.emplace_back();
         }
 
-        void resize(size_t new_size)
-        {
-            if (new_size > m_size)
-            {
+        void resize(size_t new_size) {
+            if (new_size > m_size) {
                 reserve(new_size);
 
                 // zero initialize new elements (since trivially copyable)
@@ -105,41 +89,34 @@ namespace HBE
 
                 for (size_t i = old_size; i < new_size; ++i)
                     (*this)[i] = T{};
-            }
-            else
-            {
+            } else {
                 // shrinking only changes logical size (stable storage)
                 m_size = new_size;
             }
         }
 
-        size_t size()
-        {
+        size_t size() {
             return m_size;
         }
 
-        T* data()
-        {
+        T *data() {
             if (size() > 0)
                 return pages[0].data;
             else
                 return nullptr;
         }
 
-        size_t capacity() const
-        {
+        size_t capacity() const {
             return pages.size() * PAGE_SIZE;
         }
 
-        T& operator[](size_t i)
-        {
+        T &operator[](size_t i) {
             const size_t page_index = getPageIndex(i);
             const size_t local_index = getLocalIndex(i);
             return pages[page_index][local_index];
         }
 
-        const T& operator[](size_t i) const
-        {
+        const T &operator[](size_t i) const {
             const size_t page_index = getPageIndex(i);
             const size_t local_index = getLocalIndex(i);
             return pages[page_index][local_index];
