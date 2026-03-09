0b072688406826c533205f7edcdb6f8b755735fb
Gabriel Bouchard <33035583+Goutch@users.noreply.github.com>
2026-03-09 10:35:12 -0400

    Update HandleProvider.h

--- data-structure/HandleProvider.h
+++ data-structure/HandleProvider.h
@@ -6,62 +6,61 @@
 #include "Handle.h"
 
 namespace HBE {
+    class HandleProvider {
+        RawVector<Handle> handles;
+        RawVector<uint32_t> free_handles_indices;
+        static const uint64_t VERSION_INCREMENT = 1ULL << 32;
 
+    public:
+        HandleProvider() = default;
 
-	class HandleProvider {
-		RawVector<Handle> handles;
-		RawVector<uint32_t> free_handles_indices;
-		static const uint64_t VERSION_INCREMENT = 1ULL << 32;
-	public:
-		HandleProvider() = default;
+        HandleProvider(HandleProvider &&other) {
+            free_handles_indices = std::move(other.free_handles_indices);
+            handles = std::move(other.handles);
+        }
 
-		HandleProvider(HandleProvider &&other) {
-			free_handles_indices = std::move(other.free_handles_indices);
-			handles = std::move(other.handles);
-		}
+        HandleProvider &operator=(HandleProvider &other) = delete;
 
-		HandleProvider &operator=(HandleProvider &other) = delete;
+        HandleProvider &operator=(HandleProvider &&other) noexcept {
+            free_handles_indices = std::move(other.free_handles_indices);
+            handles = std::move(other.handles);
+            return *this;
+        }
 
-		HandleProvider &operator=(HandleProvider &&other) noexcept {
-			free_handles_indices = std::move(other.free_handles_indices);
-			handles = std::move(other.handles);
-			return *this;
-		}
+        static inline uint32_t index(Handle handle) {
+            return handle & 0xFFFFFFFF;
+        }
 
-		inline uint32_t index(Handle handle) {
-			return handle & 0xFFFFFFFF;
-		}
+        inline uint32_t version(Handle handle) {
+            return (handle >> 32) & 0xFFFFFFFF;
+        }
 
-		inline uint32_t version(Handle handle) {
-			return (handle >> 32) & 0xFFFFFFFF;
-		}
+        void release(Handle handle) {
+            if (!valid(handle))
+                return;
+            handles[index(handle)] += VERSION_INCREMENT;
+            free_handles_indices.push_back(index(handle));
+        }
 
-		void release(Handle handle) {
-			if (!valid(handle))
-				return;
-			handles[index(handle)] += VERSION_INCREMENT;
-			free_handles_indices.push_back(index(handle));
-		}
+        bool valid(Handle handle) {
+            uint32_t index = this->index(handle);
+            return handles.size() > index && version(handles[index]) == version(handle);
+        }
 
-		bool valid(Handle handle) {
-			uint32_t index = this->index(handle);
-			return handles.size() > index && version(handles[index]) == version(handle);
-		}
+        size_t size() {
+            return handles.size();
+        }
 
-		size_t size() {
-			return handles.size();
-		}
-
-		Handle create() {
-			if (!free_handles_indices.empty()) {
-				Handle handle = handles[free_handles_indices[free_handles_indices.size() - 1]];
-				free_handles_indices.resize(free_handles_indices.size() - 1);
-				return handle;
-			}
-			Handle handle = 0;
-			handle = static_cast<uint64_t >(handles.size());
-			handles.push_back(handle);
-			return handle;
-		}
-	};
+        Handle create() {
+            if (!free_handles_indices.empty()) {
+                Handle handle = handles[free_handles_indices[free_handles_indices.size() - 1]];
+                free_handles_indices.resize(free_handles_indices.size() - 1);
+                return handle;
+            }
+            Handle handle = 0;
+            handle = static_cast<uint64_t>(handles.size());
+            handles.push_back(handle);
+            return handle;
+        }
+    };
 }
