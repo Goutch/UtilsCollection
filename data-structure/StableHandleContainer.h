bef699cda2595b3427214178b9293d1c6697fc09
Gabriel <gaboutch@outlook.com>
2026-03-13 16:29:51 -0400

    foreach for stableHandleCollection

--- data-structure/StableHandleContainer.h
+++ data-structure/StableHandleContainer.h
@@ -1,44 +1,75 @@
 #pragma once
+
 #include "StableVector.h"
 #include "HandleProvider.h"
 
-namespace HBE
-{
-    template <typename T, size_t PAGE_SIZE>
-    class StableHandleContainer
-    {
-        StableVector<T, PAGE_SIZE> m_stable_data;
-        HandleProvider handle_provider;
+namespace HBE {
+	template<typename T, size_t PAGE_SIZE>
+	class StableHandleContainer {
+		StableVector<T, PAGE_SIZE> m_stable_data;
+		HandleProvider handle_provider;
 
-    public:
-        StableHandleContainer() = default;
-        ~StableHandleContainer() = default;
+	public:
+		StableHandleContainer() = default;
 
-        Handle create()
-        {
-            Handle handle = handle_provider.create();
-            m_stable_data.resize(handle_provider.size());
-            return handle;
-        }
+		~StableHandleContainer() = default;
 
-        T& get(const Handle& handle)
-        {
-            return m_stable_data[handle_provider.index(handle)];
-        }
+		Handle create() {
+			Handle handle = handle_provider.create();
+			m_stable_data.resize(handle_provider.size());
+			return handle;
+		}
 
-        void release(const Handle& handle)
-        {
-            m_stable_data.erase(handle_provider.index(handle));
-        }
+		T &get(const Handle &handle) {
+			return m_stable_data[handle_provider.index(handle)];
+		}
 
-        T& operator[](Handle handle)
-        {
-            return get(handle);
-        }
+		void clear(){
+			m_stable_data.clear();
+			handle_provider.clear();
+		}
+		void release(const Handle &handle) {
+			m_stable_data.erase(handle_provider.index(handle));
+		}
 
-        bool valid(const Handle& handle)
-        {
-            return handle_provider.valid(handle);
-        }
-    };
+		T &operator[](Handle handle) {
+			return get(handle);
+		}
+
+		bool valid(const Handle &handle) {
+			return handle_provider.valid(handle);
+		}
+
+		class Iterator {
+			StableHandleContainer *container;
+			Handle index;
+
+		public:
+			Iterator(StableHandleContainer *c, size_t i)
+					: container(c), index(i) {
+			}
+
+			Iterator &operator++() {
+				++index;
+				return *this;
+			}
+
+			bool operator!=(const Iterator &other) const {
+				return index != other.index;
+			}
+
+			T &operator*() {
+				return container->m_stable_data[index];
+			}
+		};
+
+
+		Iterator begin() {
+			return Iterator(this, 0);
+		}
+
+		Iterator end() {
+			return Iterator(this, handle_provider.size());
+		}
+	};
 }
