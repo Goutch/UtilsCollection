b46e6656bff19e6dfbee4ab99563f8afdc50349f
Gabriel <gaboutch@outlook.com>
2026-09-25 16:09:46 -0400

    Update Random.h

--- Random.h
+++ Random.h
@@ -5,8 +5,13 @@
 namespace HBE {
 	class Random {
 	public:
+
 		static float floatRange(float min, float max) {
 			return min + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (max - min)));
+		}
+
+		static vec3 vec3Range(vec3 min, vec3 max) {
+			return vec3(floatRange(min.x, max.x), floatRange(min.y, max.y), floatRange(min.z, max.z));
 		}
 
 		static int intRange(int min, int max) {
